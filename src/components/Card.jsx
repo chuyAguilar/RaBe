841c1d7c1ba8c3ebabc3df9a02e50156870aa3c7
chuyAguilar <jesusaguilar32265@gmail.com>
2026-02-19 11:26:47 -0600

    quite borde de cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@
 export function Card(props) {
     const { tittle, subtittle,children, className = "" } = props
     return (
-        <div className={`bg-white max-w-sm p-6 border-2 border-solid rounded-lg shadow-xs flex flex-col items-center hover:bg-blue-200 ${className}`}>
+        <div className={`bg-white max-w-sm p-6  rounded-lg shadow-xs flex flex-col items-center hover:bg-blue-200 ${className}`}>
 
             {children}
             <h5 className="text-center mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">{tittle}</h5>
